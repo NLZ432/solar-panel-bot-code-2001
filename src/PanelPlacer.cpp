97896527720fcaefe60ebce8a5c98e3bc2def078
Nestor <nlzx432@gmail.com>
2020-10-03 05:13:30 -0400

    began state machine, added necessary functions and variables

--- src/PanelPlacer.cpp
+++ src/PanelPlacer.cpp
@@ -2,7 +2,8 @@
 
 void PanelPlacer::init()
 {
-    state = IDLE;
+    changeGoal(REMOVE_45);
+    
     Serial.begin(9600);
 
     fourbar.mount();
@@ -15,26 +16,110 @@ void PanelPlacer::init()
 
 void PanelPlacer::run()
 {
-    switch(state)
+    if (behaviorState == IDLE && buttonA.isPressed()) behaviorState = idleBuffer;
+    
+    switch(goalState)
     {
-        case IDLE:
-            if (buttonA.isPressed()) state = RUNNING;
+        case REMOVE_45:
+            remove_45_stator();
             break;
-        case RUNNING:
-            status();
-            if (!buttonA.isPressed()) state = IDLE;
+        case DEPOSIT_45:
+            deposit_45_stator();
+            break;
+        case PLACE_45:
+            place_45_stator();
             break;
         default:
             status();
     }
 }
 
+void PanelPlacer::remove_45_stator()
+{
+    switch(behaviorState)
+    {
+        case TO_PANEL:
+            //if fourbar.position(POSITION_45) <- runs motor, returns true when in position
+            //{
+                //ultrasonic.ping();
+                //float distance = ultrasonic.distance;
+                //float error = GRAB_DISTANCE - distance;
+                //if linefollower.PID(error) <- runs motor returns true when error < threshold
+                //{
+                    //stop motors;
+                    //changeBehavior(GRAB);
+                //} 
+            // }
+            break;
+        
+        case GRAB:
+        //this could probably be blocking
+            //gripper.open();
+            //chassis.driveDistance(GRIPPER_APPROACH_DISTANCE);
+            //gripper.close();
+            //while (!fourbar.position(POSITION_45 + 15)){};
+            //chassis.driveDistance(-GRIPPER_APPROACH_DISTANCE);
+            //changeGoal(PLACE_45);
+            //break;
+        default: 
+            status();
+    }
+}
+
+void PanelPlacer::deposit_45_stator()
+{
+    switch(behaviorState)
+    {
+        default:
+            status();
+    }
+}
+
+void PanelPlacer::place_45_stator()
+{
+    switch(behaviorState)
+    {
+        default:
+            status();
+    }
+}
+
+void PanelPlacer::changeGoal(GoalStates new_goal)
+{
+    goalState = new_goal;
+    switch(goalState)
+    {
+        case REMOVE_45:
+            changeBehavior(TO_PANEL);
+            break;
+        case DEPOSIT_45:
+            behaviorRegister = TO_INTERSECTION; //head to intersection after turn
+            memoryRegister = 180; //turn 180 cc
+            changeBehavior(TURN);
+            break;
+        case PLACE_45:
+            behaviorRegister = TO_INTERSECTION;
+            memoryRegister = 180;
+            changeBehavior(TURN);
+            break;
+    }
+}
+
+void PanelPlacer::changeBehavior(BehaviorStates new_behavior)
+{
+    idleBuffer = new_behavior;
+    behaviorState = STEP_MODE ? IDLE : new_behavior;
+    chassis.resetEncoders();
+}
+
 void PanelPlacer::status()
 {
     clock = millis();
 
-    Serial.print("state: ");
-    Serial.print(state);
+    Serial.print("goal: ");
+    Serial.print(goalState);
+    Serial.print("\tbehavior: ");
+    Serial.print(behaviorState);
     Serial.print("\ttime: ");
     Serial.println(clock);
 }
--- src/PanelPlacer.h
+++ src/PanelPlacer.h
@@ -5,13 +5,44 @@
 
 class PanelPlacer 
 {
+
+    bool STEP_MODE = true; //will wait for button after each behavior change
+
     Chassis chassis;
     BlueMotor fourbar;
     Rangefinder ultrasonic;
     Romi32U4ButtonA buttonA;
 
-    enum states { IDLE, RUNNING } state;
+    enum GoalStates {
+        REMOVE_45,
+        DEPOSIT_45,
+        PLACE_45,
+        REMOVE_25,
+        DEPOSIT_25,
+        PLACE_25
+    } goalState;
+    enum BehaviorStates { 
+        IDLE,
+        POSITION,
+        GRAB,
+        TURN,
+        PLACE,
+        TO_PANEL,
+        TO_INTERSECTION,
+        TO_STATION
+    } behaviorState;
+    
     unsigned long clock;
+    BehaviorStates idleBuffer;
+    BehaviorStates behaviorRegister;
+    int memoryRegister;
+
+    void remove_45_stator();
+    void deposit_45_stator();
+    void place_45_stator();
+
+    void changeBehavior(BehaviorStates new_behavior);
+    void changeGoal(GoalStates new_goal);
 
 public:
     void init();
