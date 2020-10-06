aeab32b89f1625f16fb5c6442267b2ae16d1d116
Nestor <nlzx432@gmail.com>
2020-10-06 03:58:19 -0400

    fundamentally changed how the state machine works

--- src/PanelPlacer.cpp
+++ src/PanelPlacer.cpp
@@ -2,12 +2,12 @@
 
 void PanelPlacer::init()
 {
-    changeGoal(REMOVE_45);
-    
-    Serial.begin(9600);
-
     fourbar.mount();
     ultrasonic.wake();
+
+    changeGoal(TG1);
+
+    Serial.begin(9600);
 
     delay(3);
     clock = millis();
@@ -16,100 +16,68 @@ void PanelPlacer::init()
 
 void PanelPlacer::run()
 {
-    if (behaviorState == IDLE && buttonA.isPressed()) behaviorState = idleBuffer;
     
-    switch(goalState)
-    {
-        case REMOVE_45:
-            remove_45_stator();
-            break;
-        case DEPOSIT_45:
-            deposit_45_stator();
-            break;
-        case PLACE_45:
-            place_45_stator();
-            break;
-        default:
-            status();
-    }
-}
+    BehaviorStates behavior = goalList[goalState].instructions[instNum].behavior;
+    int value = goalList[goalState].instructions[instNum].value;
 
-void PanelPlacer::remove_45_stator()
-{
-    switch(behaviorState)
+    switch(behavior)
     {
-        case TO_PANEL:
-            //if fourbar.position(POSITION_45) <- runs motor, returns true when in position
-            //{
-                //ultrasonic.ping();
-                //float distance = ultrasonic.distance;
-                //float error = GRAB_DISTANCE - distance;
-                //if linefollower.PID(error) <- runs motor returns true when error < threshold
-                //{
-                    //stop motors;
-                    //changeBehavior(GRAB);
-                //} 
-            // }
+        case POSITION:
+            if (millis() - clock > 2000)
+            {
+            fourbar.setEffort(value);
+            clock = millis();
+            Serial.println(millis());
+            next();
+            }
             break;
-        
-        case GRAB:
-        //this could probably be blocking
-            //gripper.open();
-            //chassis.driveDistance(GRIPPER_APPROACH_DISTANCE);
-            //gripper.close();
-            //while (!fourbar.position(POSITION_45 + 15)){};
-            //chassis.driveDistance(-GRIPPER_APPROACH_DISTANCE);
-            //changeGoal(PLACE_45);
-            //break;
-        default: 
-            status();
+        case IDLE: 
+            if (buttonA.isPressed()) { goalState = idleBuffer; }
+            break;
     }
-}
 
-void PanelPlacer::deposit_45_stator()
-{
-    switch(behaviorState)
-    {
-        default:
-            status();
-    }
-}
-
-void PanelPlacer::place_45_stator()
-{
-    switch(behaviorState)
-    {
-        default:
-            status();
-    }
 }
 
 void PanelPlacer::changeGoal(GoalStates new_goal)
 {
+    Serial.print("CHANGED TO GOAL ");
+    Serial.print(new_goal);
+    Serial.print("  BECAUSE INST COUNT = ");
+    Serial.println(instNum);
     goalState = new_goal;
-    switch(goalState)
-    {
-        case REMOVE_45:
-            changeBehavior(TO_PANEL);
-            break;
-        case DEPOSIT_45:
-            behaviorRegister = TO_INTERSECTION; //head to intersection after turn
-            memoryRegister = 180; //turn 180 cc
-            changeBehavior(TURN);
-            break;
-        case PLACE_45:
-            behaviorRegister = TO_INTERSECTION;
-            memoryRegister = 180;
-            changeBehavior(TURN);
-            break;
-    }
+    instNum = 0;
 }
 
-void PanelPlacer::changeBehavior(BehaviorStates new_behavior)
+void PanelPlacer::next()
 {
-    idleBuffer = new_behavior;
-    behaviorState = STEP_MODE ? IDLE : new_behavior;
-    chassis.resetEncoders();
+    instNum++;
+
+    //if this was the last instruction, switch the goal state
+    if (instNum == goalList[goalState].instructionCount)
+    {
+        switch(goalState)
+        {
+            case TG1:
+                changeGoal(TG2);
+                break;
+            case TG2:
+                fourbar.setEffort(0);
+                delay(500);
+                changeGoal(TG1);
+                break;
+        }
+    }
+    else 
+    {
+        Serial.print("CHANGED TO BEHAVIOR ");
+        Serial.println(instNum);
+    }
+
+    if (STEP_MODE) {
+        idleBuffer = goalState;
+        goalState = IDLING; 
+    }
+
 }
 
 void PanelPlacer::status()
--- src/PanelPlacer.h
+++ src/PanelPlacer.h
@@ -14,32 +14,62 @@ class PanelPlacer
     Romi32U4ButtonA buttonA;
 
     enum GoalStates {
-        REMOVE_45,
-        DEPOSIT_45,
-        PLACE_45,
-        REMOVE_25,
-        DEPOSIT_25,
-        PLACE_25
+         IDLING,
+         TG1,
+         TG2 
     } goalState;
+
     enum BehaviorStates { 
         IDLE,
         POSITION,
-        GRAB,
+        OPEN,
+        CLOSE,
         TURN,
         PLACE,
+        DRIVE_DISTANCE,
         TO_PANEL,
         TO_INTERSECTION,
         TO_STATION
     } behaviorState;
-    
+
+    struct instruction
+    {
+        BehaviorStates behavior;
+        int value; //turn degrees, position degrees, distance, the magnitude of the instruction
+    };
+
+    struct goal
+    {
+        instruction instructions[10];
+        int instructionCount;    
+        GoalStates state;
+    };
+
+    goal IDLE_INST = {
+        { {IDLE, 0} }, 1, IDLING
+    };
+
+    goal TESTGOAL1 = {
+        { {POSITION,90},
+          {POSITION,60},
+          {POSITION,30} }, 3, TG1 };
+
+    goal TESTGOAL2 = {
+        { {POSITION,-30},
+          {POSITION,-60},
+          {POSITION,-90} }, 3, TG2 };
+
+    int instNum; //index of current instruction
+    goal goalList[3] = { IDLE_INST, TESTGOAL1, TESTGOAL2 };
+
     unsigned long clock;
-    BehaviorStates idleBuffer;
-    BehaviorStates behaviorRegister;
-    int memoryRegister;
+    GoalStates idleBuffer;
 
     void remove_45_stator();
     void deposit_45_stator();
     void place_45_stator();
+
+    void next();
 
     void changeBehavior(BehaviorStates new_behavior);
     void changeGoal(GoalStates new_goal);
